58c8202042b78c17307c93eb185d8e71e6f44b74
MeutiaKhanandiya <khanandyam@gmail.com>
2020-04-15 14:04:27 +0700

    Menyelesaikan Praktikum dan Tugas Minggu11

---
+++ minggu11/src/firebase.config.js
@@ -0,0 +1,10 @@
+ export default {
+    apiKey: "AIzaSyBAKjYKmY13b8bCStcwFSVWe_DMxJj8mao",
+    authDomain: "minggu11-64aee.firebaseapp.com",
+    databaseURL: "https://minggu11-64aee.firebaseio.com",
+    projectId: "minggu11-64aee",
+    storageBucket: "minggu11-64aee.appspot.com",
+    messagingSenderId: "859530717739",
+    appId: "1:859530717739:web:ce1d47951980d96c79186e",
+    measurementId: "G-GTBVLNLLNS"
+  };

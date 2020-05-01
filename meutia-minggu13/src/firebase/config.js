31c79d44702cdc8f46fea48200e1da6c25bf7f69
MeutiaKhanandiya <khanandyam@gmail.com>
2020-05-01 18:28:31 +0700

    Menyelesaikan Minggu-13
    Modul 12

---
+++ meutia-minggu13/src/firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDL1RVBtzWVHnh9mjtkF5M43QX9VyN6154",
+    authDomain: "minggu12-c6d75.firebaseapp.com",
+    databaseURL: "https://minggu12-c6d75.firebaseio.com",
+    projectId: "minggu12-c6d75",
+    storageBucket: "minggu12-c6d75.appspot.com",
+    messagingSenderId: "546025011664",
+    appId: "1:546025011664:web:9360a6d32a6fd13746e1d5",
+    measurementId: "G-43R5J7X5NG"
+  };
+
+  export default firebaseConfig;
